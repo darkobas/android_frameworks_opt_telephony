b5af7f1ec1886a5681dfea615d3a4437cc028acc
darkobas <darkobas@gmail.com>
2015-11-03 14:56:14 +0100

    Revert "SEEMP: add more framework instrumentation"
    This reverts commit f01dc085a07ebbb7c66afbd4ed67de51821c40db.

--- src/java/android/telephony/SmsManager.java
+++ src/java/android/telephony/SmsManager.java
@@ -290,7 +290,6 @@ public final class SmsManager {
     public void sendTextMessage(
             String destinationAddress, String scAddress, String text,
             PendingIntent sentIntent, PendingIntent deliveryIntent) {
-        android.util.SeempLog.record_str(75, destinationAddress);
         sendTextMessageInternal(destinationAddress, scAddress, text,
             sentIntent, deliveryIntent, true /* persistMessageForCarrierApp*/);
     }
@@ -709,7 +708,6 @@ public final class SmsManager {
     public void sendDataMessage(
             String destinationAddress, String scAddress, short destinationPort,
             byte[] data, PendingIntent sentIntent, PendingIntent deliveryIntent) {
-        android.util.SeempLog.record_str(73, destinationAddress);
         if (TextUtils.isEmpty(destinationAddress)) {
             throw new IllegalArgumentException("Invalid destinationAddress");
         }
@@ -871,7 +869,6 @@ public final class SmsManager {
      * {@hide}
      */
     public boolean copyMessageToIcc(byte[] smsc, byte[] pdu,int status) {
-        android.util.SeempLog.record(79);
         boolean success = false;
 
         if (null == pdu) {
@@ -903,7 +900,6 @@ public final class SmsManager {
      */
     public boolean
     deleteMessageFromIcc(int messageIndex) {
-        android.util.SeempLog.record(80);
         boolean success = false;
         byte[] pdu = new byte[IccConstants.SMS_RECORD_LENGTH-1];
         Arrays.fill(pdu, (byte)0xff);
@@ -937,7 +933,6 @@ public final class SmsManager {
      * {@hide}
      */
     public boolean updateMessageOnIcc(int messageIndex, int newStatus, byte[] pdu) {
-        android.util.SeempLog.record(81);
         boolean success = false;
 
         try {
